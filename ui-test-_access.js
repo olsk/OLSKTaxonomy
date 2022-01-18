7db05b5c53c1230045c288a209e80bddac36e132
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:11 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -9,7 +9,7 @@ Object.entries({
 	OLSKTaxonomyHint: '.choices__item--choice',
 	OLSKTaxonomySuggestion: '.OLSKTaxonomySuggestion',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('OLSKTaxonomy_Access', function () {
